5bf42550798b3a12fc04a23f4e2dbb635d6fafdc
rishalkp367 <rishalkp367@gmail.com>
2026-01-07 17:49:51 +0530

    Doubly linked list

---
+++ guessing_game/target/debug/deps/cfg_if-b84f1857553d0875.d
@@ -0,0 +1,5 @@
+/home/rishal/Desktop/Rust/guessing_game/target/debug/deps/cfg_if-b84f1857553d0875.d: /home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.3/src/lib.rs
+
+/home/rishal/Desktop/Rust/guessing_game/target/debug/deps/libcfg_if-b84f1857553d0875.rmeta: /home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.3/src/lib.rs
+
+/home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.3/src/lib.rs:
---
+++ guessing_game/target/debug/deps/rand_chacha-183674b5836ad943.d
@@ -0,0 +1,7 @@
+/home/rishal/Desktop/Rust/guessing_game/target/debug/deps/rand_chacha-183674b5836ad943.d: /home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/home/rishal/Desktop/Rust/guessing_game/target/debug/deps/librand_chacha-183674b5836ad943.rmeta: /home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs:
+/home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs:
+/home/rishal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs:
